9fe4efd98336140022e6a3e12febe9a902fc3e8f
Luciano Barreto <contato.lucianobarreto@gmail.com>
2021-08-22 15:19:04 -0300

    Refatorado => adicionando comentários

--- src/components/Header/SearchBox.tsx
+++ src/components/Header/SearchBox.tsx
@@ -1,9 +1,18 @@
 import { Flex, Icon, Input } from "@chakra-ui/react";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { RiSearchLine } from "react-icons/ri";
 
+// Controlled components
+// Uncontrolled components
+
+// Imperativa ou Declarativa...
+
 export function SearchBox() {
+  // const [search, setSearch] = useState('')
+
   const searchInputRef = useRef<HTMLInputElement>(null)
+
+  // searchInputRef.current.focus()           => (Imperativo) <=
 
   return (
     <Flex
